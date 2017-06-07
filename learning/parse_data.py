6f753e0ac9711fc5b6236fa272c400f05d2dcfab
SunnySWY <apple0105@126.com>
2017-06-07 14:50:40 +0800

    休假状态默认从本地读取，本地没有就从网页API读取

--- learning/parse_data.py
+++ learning/parse_data.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, urllib.request
-import setting
+import setting, pickle
 
 '''当前路径'''
 current_path = os.getcwd()
@@ -8,6 +8,8 @@ current_path = os.getcwd()
 father_path = os.path.abspath(os.path.dirname(current_path)+os.path.sep+".")
 '''项目数据目录'''
 data_path = os.path.join(father_path, 'datas')
+'''辅助数据库'''
+helper_path = os.path.join(father_path, 'helper_data')
 
 def data_trans(data_str):
     new_str = ''
@@ -19,19 +21,18 @@ def data_trans(data_str):
 
 def parse_data(): 
     with open(os.path.join(data_path, 'Tianchi_power.csv')) as base_data:
-        return_dict, data_record = {}, {}
+        return_dict = {}
+        date_record = pickle.load(open(os.path.join(helper_path, 'holiday'), 'rb'))
         for lines in base_data.readlines()[1:]:
             date_str, user_id, el_cost = lines.split(',')
             req_date = data_trans(date_str)
-            if req_date in data_record:
-                holiday_type = data_record[req_date]
+            if req_date in date_record:
+                holiday_type = date_record[req_date]
             else: 
                 req = urllib.request.Request(setting.HOLIDAY_API+'?d='+req_date)
                 conn = urllib.request.urlopen(req)
                 holiday_type = conn.read()
-                data_record[req_date] = holiday_type
-            print(data_record)
+                date_record[req_date] = holiday_type
             return_dict[user_id] = None
-        print(data_record)
-        
+
 parse_data()

bab494f5187ffa2ef385abc3f1792b7191796071
RoyGuo <253583536@qq.com>
2017-06-08 14:57:41 +0800

    1.for みじ兄貴

--- test.py
+++ test.py
@@ -2,25 +2,21 @@
 from xgboost import XGBRegressor
 import pandas as pd
 
-oldData = pd.read_csv('./helper_data/temp/user_one.csv')
-needPredictData = pd.read_csv('./helper_data/temp/future.csv')
+oldData = pd.read_csv('./helper_data/temp/all.csv')#所有数据
+# oneData = pd.read_csv('./helper_data/temp/user_one.csv')#用户1的数据用来预测天气
+# needPredictData = pd.read_csv('./helper_data/temp/future.csv')#预测天气数据
 
-# print(oldData)
 
-#组建数据
+
+#组建天气预测数据
+# temperture_features = ['date_offset','week_day','holiday_type']
+# temperture_labels = ['avg_temp']
 #
-power_features = ['date_offset','week_day','holiday_type','avg_temp']
-power_labels = ['cost_el']
+#
+# x_train = oneData[temperture_features]
+# y_train = oneData[temperture_labels]
 
-#,'user_id'
-tempFeatures = ['date_offset','week_day','holiday_type']
-labels = ['avg_temp']
-
-
-x_train = oldData[tempFeatures]
-y_train = oldData[labels]
-
-x_test = needPredictData[tempFeatures]
+# x_test = needPredictData[temperture_features]
 
 
 
@@ -37,22 +33,38 @@ from sklearn.ensemble import RandomForestRegressor
 #     tempPredict.append(temp)
 #
 # xg_submission = pd.DataFrame({'date_offset':needPredictData['date_offset'],'week_day':needPredictData['week_day'],
-#                               'holiday_type':needPredictData['holiday_type'],'user_id':needPredictData['user_id']
+#                               'holiday_type':needPredictData['holiday_type']
 #                                  ,'avg_temp':tempPredict})
-# xg_submission.to_csv('./helper_data/predict/needPredict.csv',index=False)
+# xg_submission.to_csv('./helper_data/predict/temperaturePredict.csv',index=False)
 # print(tempPredict)
 
 
-#读取预测9月天气来训练用电量
-powerPredictData = pd.read_csv('./helper_data/predict/needPredict.csv')
+# 读取预测9月天气来训练用电量
+predictedTempertrueData = pd.read_csv('./helper_data/predict/temperaturePredict.csv') #预测后的天气数据 16/9/1-16/9/30
 
-power_x_train = oldData[power_features]
-power_y_train = oldData[power_labels]
+userIDs = oldData['user_id']
+userIDs = set(userIDs)
+# print(userIDs)
 
-power_x_test = powerPredictData[power_features]
+power_features = ['week_day','holiday_type','avg_temp']
+power_labels = ['cost_el']
+
+return_list = []
+
+for i in userIDs:
+    # print(i)
+   currentData = oldData[oldData.user_id == i]
+   print('======')
+   x_train = currentData[power_features]
+   y_train = currentData[power_labels]
+   x_test  = predictedTempertrueData[power_features]
+   xg = RandomForestRegressor()
+   xg.fit(x_train,y_train)
+   y_predict = xg.predict(x_test)
+
+# print(oldData[oldData.user_id == 1])
 
 
-xg = RandomForestRegressor()
-xg.fit(power_x_train,power_y_train)
-y_predict = xg.predict(power_x_test)
-print(y_predict)
+
+
+
